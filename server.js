c00395ffefb04e33a0f6a67a3ec3aaea978000c5
Ryan McMillan <rmcmillan@maplewellengineering.com>
2024-08-10 16:57:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-res.writeHead(200,{'Content-Type': 'text/plain'});
-res.end('NodeJS App');
-});
-server.listen(8080);
-console.log('Server is running at http://localhost:8080/');
+//var http = require('http');
+//var server = http.createServer(function (req, res) {
+//res.writeHead(200,{'Content-Type': 'text/plain'});
+//res.end('NodeJS App');
+//});
+//server.listen(8080);
+http-server -o
+//console.log('Server is running at http://localhost:8080/');
