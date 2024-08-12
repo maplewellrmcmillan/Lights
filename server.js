1e8e1f9005cb4401ce5e65a511526a37323b0749
Ryan McMillan <rmcmillan@maplewellengineering.com>
2024-08-12 17:27:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,19 @@
 //res.end('NodeJS App');
 //});
 //server.listen(8080);
-http-server -o
+//http-server -o
 //console.log('Server is running at http://localhost:8080/');
+
+const http = require('http')
+const requestListener = function (req, res) {
+    const headers = {
+        'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Max-Age': 2592000, // 30 days
+        /** add other headers as per requirement */
+    };
+    res.writeHead(200, headers);
+    res.end(JSON.stringify({"key":"value"}));
+}
+const server = http.createServer(requestListener);
+server.listen(8080);
